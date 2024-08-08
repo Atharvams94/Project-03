3ddeefb5767517c0e12f312944cec7bf881b867a
Atharva Shanware <146636925+Atharvams94@users.noreply.github.com>
2024-08-08 23:45:49 +0530

    Update Tp.py

--- Tp.py
+++ Tp.py
@@ -1,28 +1,6 @@
 import numpy as np
 
-def fft(data):
-  """
-  Performs the Fast Fourier Transform (FFT) on a given data set.
 
-  Args:
-      data: A numpy array containing the data for which to compute the FFT.
-
-  Returns:
-      A numpy array containing the frequency spectrum of the data.
-  """
-  return np.fft.fft(data)
-
-# Sample data
-data = np.random.rand(100)
-
-# Calculate FFT
-fft_result = fft(data)
-
-# Print the magnitude of the frequency spectrum (absolute values)
-print(np.abs(fft_result))
-
-def analyze_data(data_file, algorithm="default"):
-  """
   Analyzes data from a file using a specified algorithm.
 
   Args:
