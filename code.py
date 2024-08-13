113a6d4a5b6bbe49ca04b52f81482dfcb33964cd
Atharva Shanware <146636925+Atharvams94@users.noreply.github.com>
2024-08-13 22:52:26 +0530

    Update code.py

--- code.py
+++ code.py
@@ -4,17 +4,7 @@ def fft(data):
   
 data = np.random.rand(100)
 
-# Calculate FFT
-fft_result = fft(data)
 
-# Print the magnitude of the frequency spectrum (absolute values)
-print(np.abs(fft_result))
-
-def analyze_data(data_file, algorithm="default"):
-  """
-  Analyzes data from a file using a specified algorithm.
-
-  Args:
       data_file: Path to the file containing the data.
       algorithm: Algorithm to use for analysis (default or "advanced").
 
